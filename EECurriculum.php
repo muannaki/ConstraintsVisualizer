6e9336aa70a966af0074157c88f050798ec9aa8d
muannaki <ahmad.muannaki@mail.mcgill.ca>
2014-11-24 18:44:43 -0500

    modified EEC order

--- EECurriculum.php
+++ EECurriculum.php
@@ -66,8 +66,8 @@ $query_Recordset2 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Nu
 $query_Recordset3 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID > 11 AND C_ID <= 16";
 $query_Recordset4 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID > 16 AND C_ID <= 21";
 $query_Recordset5 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID > 21 AND C_ID <= 26";
-$query_Recordset6 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID IN (27, 28, 29, 31, 35, 36) ORDER BY FIELD (C_ID, 27, 28, 29, 35, 36, 31)";
-$query_Recordset7 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID IN (30, 32, 37, 38, 33, 34) ORDER BY FIELD (C_ID, 30, 37, 38, 34, 32, 33)";
+$query_Recordset6 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID > 26 AND C_ID <= 32";
+$query_Recordset7 = "SELECT courses.C_ID, courses.Course_Code, courses.Course_Number, courses.Course_Name FROM courses WHERE C_ID > 32 AND C_ID <= 38";
 
 $Recordset1 = mysql_query($query_Recordset1, $con_mcgill) or die(mysql_errno());
 $Recordset2 = mysql_query($query_Recordset2, $con_mcgill) or die(mysql_errno());
@@ -125,12 +125,12 @@ function checkSemester(semester, bx) {
 				}
 				break;
 			case 6:
-				if(cbs[i].type == 'checkbox' & (cbs[i].id == 27 | cbs[i].id == 28 | cbs[i].id == 29 | cbs[i].id == 31 | cbs[i].id == 35 | cbs[i].id == 36)) {
+				if(cbs[i].type == 'checkbox' & (cbs[i].id > 27 & cbs[i].id <= 32)) {
 					cbs[i].checked = bx.checked;
 				}
 				break;
 			case 7: 
-				if(cbs[i].type == 'checkbox' & (cbs[i].id == 30 | cbs[i].id == 32 | cbs[i].id == 37 | cbs[i].id == 38 | cbs[i].id == 33 | cbs[i].id == 34)) {
+				if(cbs[i].type == 'checkbox' & (cbs[i].id > 32 & cbs[i].id <= 38)) {
 					cbs[i].checked = bx.checked;
 				}
 				break; 
